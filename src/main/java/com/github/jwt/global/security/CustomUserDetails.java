02e9efac5e383d8fbf48136f61d1baaf28a3913d
지토지토 <iam@zito.kr>
2025-05-11 03:32:31 +0900

    feat : UserDetails에서 ROLE_ 접두사 제거

--- src/main/java/com/github/jwt/global/security/CustomUserDetails.java
+++ src/main/java/com/github/jwt/global/security/CustomUserDetails.java
@@ -15,7 +15,7 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + memberEntity.getRole()));
+        return List.of(new SimpleGrantedAuthority(memberEntity.getRole()));
     }
 
     @Override
