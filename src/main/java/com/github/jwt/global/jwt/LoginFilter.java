9c5b78a51a77e90989f30c45c6ae81fec00b7ca6
지토지토 <iam@zito.kr>
2025-05-11 03:31:30 +0900

    feat : 커스텀 로그인 필터

---
+++ src/main/java/com/github/jwt/global/jwt/LoginFilter.java
@@ -0,0 +1,48 @@
+package com.github.jwt.global.jwt;
+
+import com.github.jwt.global.security.CustomUserDetails;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, password, null);
+
+        return authenticationManager.authenticate(auth);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        CustomUserDetails userDetails = (CustomUserDetails) authResult.getPrincipal();
+
+        String username = userDetails.getUsername();
+        String role = userDetails.getAuthorities().stream().findFirst().get().toString();
+        Long expired = 30 * 60 * 1000L;  // 30분 (1,800초)
+        String token = jwtUtil.generateToken(username, role, expired);
+
+
+        response.addHeader("Authorization", "Bearer " + token);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        response.setStatus(401);
+    }
+}
