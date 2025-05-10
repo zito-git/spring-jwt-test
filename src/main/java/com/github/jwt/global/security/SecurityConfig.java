fadff3b67d965dc609bc837055ae15543c8e48ad
지토지토 <iam@zito.kr>
2025-05-11 03:33:00 +0900

    feat : SecurityConfig 필터 추가

--- src/main/java/com/github/jwt/global/security/SecurityConfig.java
+++ src/main/java/com/github/jwt/global/security/SecurityConfig.java
@@ -1,7 +1,13 @@
 package com.github.jwt.global.security;
 
+import com.github.jwt.global.jwt.JwtFilter;
+import com.github.jwt.global.jwt.JwtUtil;
+import com.github.jwt.global.jwt.LoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -9,10 +15,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtUtil jwtUtil;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -30,6 +45,8 @@ public class SecurityConfig {
                         .requestMatchers("/v1/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/", "/v0/**").permitAll()
                         .anyRequest().authenticated())
+                .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class)
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(sess -> sess
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
