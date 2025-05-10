44799d38dd5b4dd8e4d9cc98acc6a80f5405116f
지토지토 <iam@zito.kr>
2025-05-11 03:31:21 +0900

    feat : JwtUtil -> 토큰생성, 유저확인, 역할확인, 만료시간 확인

---
+++ src/main/java/com/github/jwt/global/jwt/JwtUtil.java
@@ -0,0 +1,43 @@
+package com.github.jwt.global.jwt;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private SecretKey secretKey;
+
+    public JwtUtil(@Value("${spring.jwt.secret}") String key) {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(key));
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    public Boolean isTokenExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String generateToken(String username, String role, Long expiration) {
+        String jwt = Jwts.builder()
+                .claim("username", username)
+                .claim("role", "ROLE_" + role)
+//                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secretKey)
+                .compact();
+        return jwt;
+    }
+}
